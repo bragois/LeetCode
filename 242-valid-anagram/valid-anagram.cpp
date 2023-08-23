04dd1b712b3d0fbc3e702309e9e960a219fde1b2
bragois <50234384+bragois@users.noreply.github.com>
2023-08-23 15:26:33 +0200

    Time: 16 ms (26.74%) | Memory: 7.4 MB (47.56%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mp;
-        if(s.length() != t.length()){
+        if (s.length() != t.length()){
             return false;
         }
-        for(int idx = 0; idx < s.length(); idx++){
-            if(mp.find(s[idx]) == mp.end()){
-                mp.insert(make_pair(s[idx], 1));
-            }
-            else{
-                mp[s[idx]]++;
-            }
-        }
-
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
         for(int idx = 0; idx < t.length(); idx++){
-            if(mp.find(t[idx]) == mp.end()){
+            if(s[idx] != t[idx]){
                 return false;
             }
-            else{
-                mp[t[idx]]--;
-                if(mp[t[idx]] == 0){
-                    mp.erase(t[idx]);
-                }
-            }
         }
-        return (mp.empty());
+        return true;
     }
 };
